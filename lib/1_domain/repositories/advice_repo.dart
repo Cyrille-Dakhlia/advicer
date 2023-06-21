6338d7a7ed97585fb4c19c77620443fcb8b68286
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2023-06-21 17:07:22 +0200

    feat: handle failures at domain layer

--- lib/1_domain/repositories/advice_repo.dart
+++ lib/1_domain/repositories/advice_repo.dart
@@ -1,5 +1,7 @@
 import 'package:adviser/1_domain/entities/advice_entity.dart';
+import 'package:adviser/1_domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class AdviceRepo {
-  Future<AdviceEntity> getAdviceFromDataSource();
+  Future<Either<AdviceEntity, Failure>> getAdviceFromDataSource();
 }
--- lib/1_domain/usecases/advice_usecases.dart
+++ lib/1_domain/usecases/advice_usecases.dart
@@ -1,10 +1,14 @@
 import 'package:adviser/1_domain/entities/advice_entity.dart';
+import 'package:adviser/1_domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
 
 class AdviceUseCases {
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<AdviceEntity, Failure>> getAdvice() async {
     // business logic should be done here
     await Future.delayed(const Duration(seconds: 3));
-    return const AdviceEntity(
-        advice: 'Fake advice fasely retrieved from fake API.', id: 1);
+    return left(const AdviceEntity(
+        advice: 'Fake advice fasely retrieved from fake API.', id: 1));
+    // we return right(...) when we get a failure from the repository
+    // return right(ServerFailure());
   }
 }
