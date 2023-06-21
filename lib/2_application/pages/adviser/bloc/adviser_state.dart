3b1767d49e3fcb42c76262e11e92706da87f87f3
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2023-06-21 11:14:01 +0200

    feat: add equatable dependency

--- lib/2_application/pages/adviser/bloc/adviser_state.dart
+++ lib/2_application/pages/adviser/bloc/adviser_state.dart
@@ -1,7 +1,10 @@
 part of 'adviser_bloc.dart';
 
 @immutable
-sealed class AdviserState {}
+sealed class AdviserState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class AdviserInitial extends AdviserState {}
 
@@ -9,12 +12,16 @@ final class AdviserLoadInProgress extends AdviserState {}
 
 final class AdviserLoadSuccess extends AdviserState {
   final String advice;
-
   AdviserLoadSuccess({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
 }
 
 final class AdviserLoadFailure extends AdviserState {
   final String message;
-
   AdviserLoadFailure({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
